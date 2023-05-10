172503d4f8065d2945fd78aa014196b30c1bc5f1
dolkslab <dolkslab@gmail.com>
2023-05-10 15:11:07 +0200

    yes

--- glider_main.py
+++ glider_main.py
@@ -1,6 +1,28 @@
 import numpy as np
+#import pandas as pd
 import matplotlib.pyplot as plt
 import isacalc
+import scipy.stats
+
+
+glider_data = np.genfromtxt('sailplane_data.csv',delimiter=',')[1:,2:].T/50
+def glider_statistics(val, output, inp="OEW"):
+		
+		names = ["OEW","MTOW","Mwing","Mfus","Mhstab","b","AR","S","S/W","cbar"]
+		inp_indx = names.index(inp)
+		output_indx = names.index(output)
+		print(glider_data)		
+		linfit = scipy.stats.linregress(glider_data[inp_indx], glider_data[output_indx])
+		print(linfit.slope, linfit.intercept, linfit.rvalue)
+		OEW_range = np.arange(0, 10, 0.1)
+
+		#plt.plot(glider_data[inp_indx], glider_data[output_indx], marker='o', linestyle="None")
+		#plt.plot(OEW_range, OEW_range*linfit.slope+linfit.intercept)
+		#plt.show()
+		return linfit.intercept+linfit.slope*val
+
+print(glider_statistics(3, "MTOW"))
+
 
 W = 5 #kg
 h_max = 30000
@@ -29,4 +51,8 @@ plt.plot(SS, v_from_S(SS, rho_high), SS, v_from_S(SS, rho_sl))
 plt.ylabel("velocity")
 plt.xlabel("Wing surface area")
 plt.show()
+
+
+
+
 
