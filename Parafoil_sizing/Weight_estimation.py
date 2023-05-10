6671826b27a2ce3746327a0ec01a24126fb9d9ea
mcotte <mcotte@student.tudelft.nl>
2023-05-10 16:04:22 +0200

    Create Weight_estimation.py

---
+++ Parafoil_sizing/Weight_estimation.py
@@ -0,0 +1,14 @@
+
+
+# Snowflake parafoil data
+
+M_sfdry = 1.95 # [kg] - Snowflake dry mass
+C_bat = 2100 # [mAh] - Battery capacity
+P_full = 750 # [mA] - Power consumption (controls + downlink)
+P_autopilot = 250 # [mA] - Power consumption (controls only)
+V_y = 3.66 # m/s - descend rate
+V_x = 7.2 # m/s - forward speed
+glide_ratio = 2/1 # -
+R_min = 15.2 # m - Minimum turn radius
+
+
