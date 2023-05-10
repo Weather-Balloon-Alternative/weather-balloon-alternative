5dfef093efb8462f9c971a7e8926a418120030cb
mcotte <mcotte@student.tudelft.nl>
2023-05-10 17:16:18 +0200

    Update Weight_estimation.py

--- Weight_estimation.py
+++ Weight_estimation.py
@@ -1,7 +1,6 @@
-
+from Balloon_with_motor import *
 
 # Snowflake parafoil data
-
 M_sfdry = 1.95 # [kg] - Snowflake dry mass
 C_bat = 2100 # [mAh] - Battery capacity
 P_full = 750 # [mA] - Power consumption (controls + downlink)
@@ -11,4 +10,11 @@ V_x = 7.2 # m/s - forward speed
 glide_ratio = 2/1 # -
 R_min = 15.2 # m - Minimum turn radius
 
+# Other parameters
+M_pay = 2. # kg - Payload mass
+M_parachute = 0.190 # kg - Parachute mass
 
+# Balloon weight estimation
+m_tot = M_sfdry + M_pay + M_parachute
+M_balloon = balloon_mass_update(m_tot)
+print(M_balloon)
