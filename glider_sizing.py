0c393105bb030226b63876a5cf6421bfd5758272
dolkslab <dolkslab@gmail.com>
2023-05-16 09:02:13 +0200

    add different mass estimation method

--- glider_sizing.py
+++ glider_sizing.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import isacalc
 import scipy.stats
 import scipy.integrate
+import scipy.optimize
 import blimb_calc
 
 
@@ -27,26 +28,73 @@ def glider_statistics(val, output, inp="OEM"):
 
 def balloon_weight(payload):
 	return 0.316*payload + 0.261
-		
 
-print(glider_statistics(2, "OEM", "Mpl"))
 
-if __name__ == '__main__':
-	MTOm = 10 #kg
 
-	m_pl = 2
+
+def second_estimation(payload):
 	h_max = 33000 #maximum height to be reached by balloon
 	h_level_flight = 25000 #height at wich steady gliding flight begins
 	h_buffer = 500 #the glider should reach the target with this height remaining, to account for extra go around, inneficiencies etc 
 
 	CD0 = 0.02
-	e = 0.9
-	AR = 15
+	e = 0.85
+	AR = 4.5
+	wing_loading = 30
+	wing_frac_ar = 0.035
+	fus_total_frac = 0.2
+
 
 	M = {'H2':2.015894, 'He':4.0026022} 
+	MTOm = 10 #kg
+
+
+	carry_balloon = True
+
+	for i in range(100):
+		m_balloon = balloon_weight(MTOm)
+		m_fus = MTOm*fus_total_frac
+		m_wing = wing_frac_ar*AR*MTOm
+		MTOm = m_balloon + m_fus + m_wing + m_pl
+		OEM = m_fus+m_wing
+	
+
+	W = MTOm*9.81
+	print("MTOM: {}, OEM: {}, balloon:{}, gas amt: {} [kg]".format(MTOm, OEM, m_balloon, 1))
+	
+
+def CD0AR_plot(glide_range, h_start, h_end):
+	e = 0.8
+	drag_ratio = 1
+	glide_ratio = glide_range/(h_start-h_end)
+	CLopt = lambda CD0, AR: ((np.pi*e*AR*CD0)**0.5)
+	f = lambda CD0, AR: CLopt(CD0, AR)/((CD0 + (CLopt(CD0, AR)**2)/(np.pi*AR*e))*drag_ratio) - glide_ratio
+	ARf = lambda CD0: scipy.optimize.fsolve(lambda x: f(CD0, x), 5)[0]
+
+	print(ARf(0.02))
+	CD0s = np.arange(0.01, 0.04, 0.001)
+	ARs = []
+	for i in CD0s:
+		ARs.append(ARf(i))
+
+	plt.plot(CD0s, ARs)
+	plt.show()
+
+
+def first_estimation(payload):
+	h_max = 33000 #maximum height to be reached by balloon
+	h_level_flight = 25000 #height at wich steady gliding flight begins
+	h_buffer = 500 #the glider should reach the target with this height remaining, to account for extra go around, inneficiencies etc 
+
+	CD0 = 0.02
+	e = 0.85
+	AR = 4.5
+
+	M = {'H2':2.015894, 'He':4.0026022} 
+	MTOm = 10 #kg
 	
 	print(glider_statistics(1, "OEM", "Mpl"))
-	carry_balloon = False
+	carry_balloon = True
 
 	for i in range(100):
 		#V_balloon = blimb_calc.calculate_required_volume(MTOm, h_max, M["H2"])
@@ -55,15 +103,13 @@ if __name__ == '__main__':
 		OEM = glider_statistics(m_pl+m_balloon*carry_balloon, "OEM", "Mpl")
 		#OEM = (m_pl+m_balloon*carry_balloon) * 0.25
 		MTOm = m_pl+m_balloon + OEM
-	
-	
-
 	W = MTOm*9.81
+	print(glider_statistics(OEM, "Mwing", "OEM"), "mwing")
 
 	V_balloon = blimb_calc.calculate_required_volume(MTOm, h_max, M["H2"])
 	m_gas = V_balloon*blimb_calc.gas_density(M["H2"], h_max)
 
-	CL = (np.pi*e*AR*CD0)*0.5
+	CL = (np.pi*e*AR*CD0)**0.5
 	CD = CD0 + (CL**2)/(np.pi*AR*e)
 	
 	rho_high =  isacalc.isa(h_max)['dens']
@@ -86,19 +132,20 @@ if __name__ == '__main__':
 
 	print("MTOM: {}, OEM: {}, balloon:{}, gas amt: {} [kg]".format(MTOm, OEM, m_balloon, m_gas))
 	print("Wing surface: {} [m2], Wing span: {} [m], speed at top altitude: {} [m/s]".format(S, b, v))
-
-	print("Glide ratio: {} [-], Range: {} [km], Flight time: {} [h]".format(glide_ratio,glide_range/1000,flight_time/3600))
-
+	print("C_L: {} [-], Glide ratio: {} [-], Range: {} [km], Flight time: {} [h]".format(CL, glide_ratio,glide_range/1000,flight_time/3600))
 
 
-	#vv = np.arange(10, 100, 0.1)
-	#SS = np.arange(0.3, 3, 0.01)
+print(glider_statistics(2, "OEM", "Mpl"))
 
-	#plt.plot(vv, S(vv, rho_high), vv, S(vv, rho_sl))
-	#plt.plot(SS, v_from_S(SS, rho_high), SS, v_from_S(SS, rho_sl))
-	#plt.ylabel("velocity")
-	#plt.xlabel("Wing surface area")
-	#plt.show()
+if __name__ == '__main__':
+	m_pl = 2
+	first_estimation(m_pl)
+	CD0AR_plot(300, 25, 0.5)
+	second_estimation(m_pl)
+
+	
+
+
 
 
 
