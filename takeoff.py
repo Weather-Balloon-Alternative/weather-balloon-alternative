ca7af65973d3de6b414c8af25d05b4f3027358bd
MennoWijkhuizen <95078439+MennoWijkhuizen@users.noreply.github.com>
2023-05-17 10:06:25 +0200

    making functions for fixed wing sizing

--- takeoff.py
+++ takeoff.py
@@ -1,6 +1,7 @@
 import ambiance
 import numpy as np
-arr = np.arange(0,33000,1)
+import matplotlib.pyplot as plt
+arr = np.arange(0,30500,1)
 S = 2.0
 sos= ambiance.Atmosphere(arr).speed_of_sound
 V = sos*0.8
@@ -15,12 +16,13 @@ D = 0.5*rho*V**2*S*C_D_0
 I_SP = 330
 T_array=[]
 for i in range(len(arr)):
-    T = D[i]*W*t[i]
+    T = D[i]+W
     m_dot = T/(I_SP*9.81)
     W -= m_dot*t[i]*9.81
     T_array.append(T)
 print("final structure+payload weight: ", W,"[N]")
 print("maximum thrust required: ",max(T_array),"[N]")
+print("rocket size required: ",max(T_array)/180,"[N]")
 
 
 
